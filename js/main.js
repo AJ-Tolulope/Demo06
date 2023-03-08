cd763e8ad47158d29474b766643b3e5cfa24ab04
ajalatolulope <84034486+AJ-Tolulope@users.noreply.github.com>
2023-03-08 03:22:33 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+new Swiper('.swiper', {
+    loop: true,
+
+    slidesPerView: 1,
+    spaceBetween: 10,
+
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false
+    }
+  
+});
